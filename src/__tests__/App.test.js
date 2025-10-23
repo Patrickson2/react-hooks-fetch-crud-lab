889e142f3f4dd613d9a0231cf0a42abac9efa6e1
Patrickson2 <thairupatrickson@gmail.com>
2025-10-24 00:06:21 +0300

    made changes

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -84,11 +84,11 @@ test("updates the answer when the dropdown is changed", async () => {
 
   fireEvent.change(screen.queryAllByLabelText(/Correct Answer/)[0], {
     target: { value: "3" },
-  });
+  })
 
   expect(screen.queryAllByLabelText(/Correct Answer/)[0].value).toBe("3");
 
-  rerender(<App />);
+  rerender(<App />)
 
   expect(screen.queryAllByLabelText(/Correct Answer/)[0].value).toBe("3");
-});
+})
